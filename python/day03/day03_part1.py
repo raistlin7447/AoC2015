8d25a219fcfb31f7c662fe7497bb3bfe352984bc
Kris Austin <kris.austin@draft2digital.com>
2024-12-26 15:06:51 -0600

    Day 03

---
+++ python/day03/day03_part1.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+houses = defaultdict(int)
+location = [0, 0]
+houses[tuple(location)] += 1
+
+for direction in open("day03_input.txt").read():
+    match direction:
+        case "^":
+            location[0] += 1
+        case "v":
+            location[0] -= 1
+        case ">":
+            location[1] += 1
+        case "<":
+            location[1] -= 1
+    houses[tuple(location)] += 1
+
+print(len([i for i in houses.values() if i > 0]))
---
+++ python/day03/day03_part2.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+houses = defaultdict(int)
+houses[(0, 0)] += 2
+santa_location = [0, 0]
+robot_location = [0, 0]
+santa_turn = True
+
+for direction in open("day03_input.txt").read():
+    if santa_turn:
+        location = santa_location
+    else:
+        location = robot_location
+    santa_turn = not santa_turn
+
+    match direction:
+        case "^":
+            location[0] += 1
+        case "v":
+            location[0] -= 1
+        case ">":
+            location[1] += 1
+        case "<":
+            location[1] -= 1
+    houses[tuple(location)] += 1
+
+print(len([i for i in houses.values() if i > 0]))
