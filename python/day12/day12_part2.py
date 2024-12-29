3608b36be75079d24551c9fcb37726443dea8789
Kris Austin <kris.austin@draft2digital.com>
2024-12-29 17:05:54 -0600

    Day 12

---
+++ python/day12/day12_part2.py
@@ -0,0 +1,16 @@
+import json
+
+json_input = json.loads(open("day12_input.txt").read())
+
+def item_generator(json_input):
+    match json_input:
+        case int():
+            yield json_input
+        case dict():
+            for k, v in json_input.items():
+                yield from item_generator(v)
+        case list():
+            for item in json_input:
+                yield from item_generator(item)
+
+print(sum(item_generator(json_input)))
